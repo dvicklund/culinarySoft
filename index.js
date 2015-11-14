35ce618c48e83f2669fa7e4efb876b2538bf40c8
Jack Sneed <sneed.jack@gmail.com>
2015-11-14 03:13:31 -0800

    Fix package.json and remove multer

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var clientRouter = require(__dirname + "/routes/clientRouter");
 var vendorRouter = require(__dirname + "/routes/vendorRouter");
 var bodyParser = require('body-parser');
-var multer = require('multer');
 
 mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/productdb");
 
@@ -13,7 +12,6 @@ app.use('/client', clientRouter);
 app.use('/vendor', vendorRouter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(multer);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server listening on port " + (process.env.PORT || 3000));
