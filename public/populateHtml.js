072c2b478cf854b833845f9b0cc0216edc15ed3a
David Vicklund <david@vicklund.com>
2015-11-16 12:36:35 -0800

    damn it stupid

---
+++ public/populateHtml.js
@@ -0,0 +1,18 @@
+var htmlString = "";
+
+$.get('/vendor/product', function(data) {
+  $headers = $("#listHeaders");
+  $list = $("#listBody");
+  var htmlString = '';
+  data.forEach(function(curr, i, arr) {
+    htmlString += '<tr><td>';
+    for(var i = 1; i < Object.keys(curr).length - 1; i++) {
+      i === 2 ? htmlString += numeral(Number(curr[Object.keys(curr)[i]])).format('$0,0.00') + ' /</td><td>' 
+              : htmlString += curr[Object.keys(curr)[i]] + "</td><td>";
+    }
+    htmlString += '</td></tr>';
+  });
+  htmlString += data + '</td></tr>';
+  $list.append(htmlString);
+});
+
