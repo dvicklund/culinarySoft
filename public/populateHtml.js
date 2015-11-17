4d7dacbef5ee4749148ab094adb83a31bf36309d
David Vicklund <david@vicklund.com>
2015-11-17 11:50:12 -0800

    slight syntax cleanup

--- public/populateHtml.js
+++ public/populateHtml.js
@@ -5,8 +5,8 @@ $.get('/vendor/product', function(data) {
   $list = $("#listBody");
   var htmlString = '';
   data.forEach(function(curr, i, arr) {
-    htmlString += '<tr id="' + curr["_id"] + '">';
-    htmlString += '<td><form action="/vendor/product/' + curr["_id"] + '" method="POST"><input type="submit" value="Remove"></input></form></td><td>';
+    htmlString += '<tr id="' + curr._id + '">';
+    htmlString += '<td><form action="/vendor/product/' + curr._id + '" method="POST"><input type="submit" value="Remove"></input></form></td><td>';
     for(var j = 1; j < Object.keys(curr).length - 1; j++) {
       j === 2 ? htmlString += numeral(Number(curr[Object.keys(curr)[j]])).format('$0,0.00') + ' /</td><td>' 
               : htmlString += curr[Object.keys(curr)[j]] + '</td><td>';
