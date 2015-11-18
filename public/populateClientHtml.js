4a8dc25ff619b3bcbd1cc9d6f67e4c425f45dd92
Jack Sneed <sneed.jack@gmail.com>
2015-11-18 12:38:00 -0800

    Add search to client

--- public/populateClientHtml.js
+++ public/populateClientHtml.js
@@ -1,3 +1,4 @@
+
 $.get("/client/product", function(data) {
   // Grab product list container tbody element
   $list = $("#listBody");
@@ -30,3 +31,17 @@ $.get("/client/product", function(data) {
   $('#UPN').val(counter);
 });
 
+// Display search results function (sans button)
+var displayResults = function(data) {
+  $result = $('#searchResult');
+  var htmlString = '';
+  data.forEach(function(curr, i, arr) {
+    htmlString += '<tr id="' + curr._id + '">';
+    for(var j = 1; j < Object.keys(curr).length - 1; j++) {
+      j === 2 ? htmlString += '<td>' + numeral(Number(curr[Object.keys(curr)[j]])).format('$0,0.00') + ' /</td>'
+              : htmlString += '<td>' + curr[Object.keys(curr)[j]] + '</td>';
+    }
+    htmlString += '</tr>';
+  });
+  $result.html(htmlString);
+};
--- routes/clientRouter.js
+++ routes/clientRouter.js
@@ -17,8 +17,7 @@ clientRouter.post('/products', urlencodedParser, function(req, res) {
   var nameText = req.body.nameText;
   Product.find({name: nameText}, function(err, data) {
     if(err) handleError(err, res);
-
-    res.send(JSON.stringify(data));
+    res.send(data);
   });
 });
 
