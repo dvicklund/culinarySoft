a9994dbe89b19490559281348fbc0d55254e560a
David Vicklund <david@vicklund.com>
2015-11-18 13:51:34 -0800

    add dummy html files and modify test.js

--- test/test.js
+++ test/test.js
@@ -1,45 +1,55 @@
-  var chai = require('chai');
+var chai = require('chai');
 var chaihttp = require('chai-http');
-chai.use(chaihttp);
-var expect = chai.expect;
-
-process.env.MONGOLAB_URI = "mongodb://localhost/productdb";
-require(__dirname + '/../index'); // Starts server for testing
 var mongoose = require('mongoose');
 var Product = require(__dirname + '/../models/productModel');
 
-/*describe('client router', function() {
-  it('should be able to view vendor lists', function() {
+// Starts server for testing using test db to avoid modifying real files
+process.env.MONGOLAB_URI = "mongodb://localhost/testproductdb";
+var server = require(__dirname + '/../index');
 
+var expect = chai.expect;
+var should = chai.should();
+chai.use(chaihttp);
+
+describe('client router', function() {
+//   it('should be able to view vendor lists', function() {
+
+//   });
+
+//   it('should be able to make a new list', function() {
+
+//   });
+
+//   it('should be able to add a selected vendor item to a list', function() {
+
+//   });
+
+//   it('should be able to remove a vendor item from a list', function() {
+
+//   });
+
+  it('should list all products on get /client/product', function(done) {
+    chai.request('localhost:3000')
+      .get('/client/product')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.an('object');
+        res.body.should.be.an('array');
+        done();
+      });
   });
 
-  it('should be able to make a new list', function() {
+//   it('should be able to search for a specific product', function() {
 
-  });
-
-  it('should be able to add a selected vendor item to a list', function() {
-
-  });
-
-  it('should be able to remove a vendor item from a list', function() {
-
-  });
-
-  it('should get a list of products on a get request', function() {
-
-    });
-
-  it('should be able to search for a specific product', function() {
-
-  });
-});*/
+//   });
+});
 
 describe('vendor router', function() {
-   after(function(done) {
-    mongoose.connection.db.dropDatabase(function() {
-      done();
-    });
-  });
+  //  after(function(done) {
+  //   mongoose.connection.db.dropDatabase(function() {
+  //     done();
+  //   });
+  // });
 
   it('should be able to add a product', function(done) {
     var productData = {name: 'test product'};
@@ -55,33 +65,44 @@ describe('vendor router', function() {
       });
   });
 
-  /*describe('tests which require a product in db', function() {
-    beforeEach(function(done) {
-      (new Product({name: 'test product'})).save(function(err, data) {
-        expect(err).to.eql(null);
-        this.products = data;
+  it('should list all products on get /vendor/product', function(done) {
+    chai.request('localhost:3000')
+      .get('/vendor/product')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.an('object');
+        res.body.should.be.an('array');
         done();
-      }.bind(this));
-    });
+      });
+  });
 
-    it('should be able to update a product', function() {
-      chai.request('localhost:3000')
-        .patch('/vendor/product' + this.products._id)
-        .send({name: 'a different product name'})
-        .end(function(err, res) {
-          expect(err).to.eql(null);
-          done();
-        });
-    });
+  // describe('tests which require a product in db', function() {
+  //   beforeEach(function(done) {
+  //     (new Product({name: 'test product'})).save(function(err, data) {
+  //       expect(err).to.eql(null);
+  //       this.products = data;
+  //       done();
+  //     }.bind(this));
+  //   });
 
-    it('should be able to remove a product', function() {
-      chai.request('localhost:3000')
-        .delete('vendor/product' + this.product._id)
-        .end(function(err, res) {
-          expect(err).to.eql(null);
-          expect(res.body.msg).to.eql('success!');
-          done();
-        });
-    });
-  });*/
+  //   it('should be able to update a product', function() {
+  //     chai.request('localhost:3000')
+  //       .patch('/vendor/product' + this.products._id)
+  //       .send({name: 'a different product name'})
+  //       .end(function(err, res) {
+  //         expect(err).to.eql(null);
+  //         done();
+  //       });
+  //   });
+
+  //   it('should be able to remove a product', function() {
+  //     chai.request('localhost:3000')
+  //       .delete('vendor/product' + this.product._id)
+  //       .end(function(err, res) {
+  //         expect(err).to.eql(null);
+  //         expect(res.body.msg).to.eql('success!');
+  //         done();
+  //       });
+  //   });
+  // });
 });
