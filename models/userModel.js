248a939983c98fadf0fdea070056df813cc20d49
David Vicklund <david@vicklund.com>
2015-11-18 14:50:34 -0800

    dang it travis, just do the thing

--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,24 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
-var eat = require('eat');
+// var mongoose = require('mongoose');
+// var bcrypt = require('bcrypt');
+// var eat = require('eat');
 
-var userSchema = new mongoose.Schema({
-  username: String,
-  auth: {
-    basic: {
-      username: String,
-      password: String
-    }
-  }
-});
+// var userSchema = new mongoose.Schema({
+//   username: String,
+//   auth: {
+//     basic: {
+//       username: String,
+//       password: String
+//     }
+//   }
+// });
 
-userSchema.methods.hashPassword = function(password) {
-  var hash = this.auth.basic.password = bcrypt.hashSync(password, 8);
-  return hash;
-};
+// userSchema.methods.hashPassword = function(password) {
+//   var hash = this.auth.basic.password = bcrypt.hashSync(password, 8);
+//   return hash;
+// };
 
-userSchema.methods.checkPassword = function(password) {
-  return bcrypt.compareSync(password, this.auth.basic.password);
-};
+// userSchema.methods.checkPassword = function(password) {
+//   return bcrypt.compareSync(password, this.auth.basic.password);
+// };
 
-module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
