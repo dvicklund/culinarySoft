de029559f821b649e8879261504c9ffac5123579
David Vicklund <david@vicklund.com>
2015-11-19 23:19:26 -0800

    removing auth tests

--- public/login.js
+++ public/login.js
@@ -4,14 +4,15 @@ $(function() {
 
   $('#passwordInput').keypress(function(e) {
     if(e.which == 13) {
-      window.location.replace("/../vendor.html");
-      // $.ajax({
-      //   method: 'GET',
-      //   url: '/vendor/', 
-      //   headers: {
-      //     authorization: "Basic " + btoa($('#usernameInput').val() + ":" + $('#passwordInput').val())
-      //   }
-      // });
+      $.ajax({
+        method: 'GET',
+        url: '/auth/signin', 
+        headers: {
+          authorization: "Basic " + btoa($('#usernameInput').val() + ":" + $('#passwordInput').val())
+        }
+      }).done(function(data) {
+        window.location.replace("/../vendor.html");
+      });
     }
   });
 });
