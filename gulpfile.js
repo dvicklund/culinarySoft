e6d74ae83784d0ffe5723faf19e7ddda3290ba91
David Vicklund <david@vicklund.com>
2016-01-02 15:24:01 -0800

    change background color of tables to white

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-var appFiles = ['index.js', 'lib/**/*.js', 'routes/**/*.js', 'public/**/*.js'];
-var testFiles = ['./test/**/*.js'];
+var webpack = require('webpack-stream');
+
+var files = {
+  js: ['index.js', 'lib/**/*.js', 'routes/**/*.js', 'public/**/*.js'],
+  html: ['public/**/*.html'],
+  css: ['public/**/*.css'],
+  scss: ['public/**/*.scss', 'public/**/*.sass'],
+  test: ['./test/**/*.js']
+};
+
+gulp.task('build', function() {
+  return gulp.src('www/js/app.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    })).pipe(gulp.dest('test/'));
+});
 
 gulp.task('jshint:test', function(){
-  return gulp.src(testFiles)
+  return gulp.src(files.test)
     .pipe(jshint({
       node: true,
       globals: {
@@ -19,18 +35,25 @@ gulp.task('jshint:test', function(){
 });
 
 gulp.task('jshint:app', function(){
-  return gulp.src(appFiles)
+  return gulp.src(files.js)
     .pipe(jshint({
     })).pipe(jshint.reporter('default'));
 });
 
 gulp.task('mocha:test', function(){
-  return gulp.src(testFiles)
+  return gulp.src(files.test)
     .pipe(mocha({
       reporter: 'nyan'
     }));
 });
 
+gulp.task('watch:html', function() {
+  gulp.watch(files.html, []);
+});
+gulp.task('watch:css', function() {});
+gulp.task('watch:js', function() {});
+
+gulp.task('watch', []);
 gulp.task('jshint', ['jshint:test', 'jshint:app']);
 gulp.task('mocha', ['mocha:test']);
 gulp.task('default', ['jshint', 'mocha:test']);
